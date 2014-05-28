e91d56f9a604c46d95c35001ced1b45aa13f3ed5
亚会 <@192.168.10.48>
2014-05-28 16:35:12 +0800

    xxx

--- JavaDemo/src/com/demo/Demo1.java
+++ JavaDemo/src/com/demo/Demo1.java
@@ -1,8 +1,23 @@
 package com.demo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("3333");
+		long l = 1400487114;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date = new Date(l);
+		String str = format.format(date);
+		try {
+			Date date1 = format.parse(str);
+			System.out.println(date1.getYear());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		System.out.println(str);
 	}
 }
